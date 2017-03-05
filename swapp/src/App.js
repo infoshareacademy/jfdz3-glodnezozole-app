31c4bfa5ce106a5bea44f59bf8b6d79ecdfe92a5
Paula Tomaszewska <paulatomaszewska0@gmail.com>
2017-03-05 20:25:10 +0100

    ogólne stylowanie - poprawka

--- swapp/src/App.js
+++ swapp/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import FunctionList from './function_list';
+
 class App extends Component {
   render() {
     return (
-        <FunctionList/>
+        <div></div>
     );
   }
 }
