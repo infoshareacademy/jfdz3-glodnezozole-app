9b9740e47348e6f4a8de7db0fda13a910f290137
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2017-03-16 20:36:37 +0100

    JFDZ3GZ-64 about us section done, new bootstrap dependecy added for bootstrap class names support

--- swapp/src/App.js
+++ swapp/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import FunctionList from './function_list';
+import AboutUs from './about_us/about_us';
 class App extends Component {
     render() {
         return (
-            <FunctionList/>
+            <div>
+                <FunctionList/>
+                <AboutUs/>
+            </div>
         );
     }
 }
