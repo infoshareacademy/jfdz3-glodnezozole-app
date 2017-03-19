cef9155e057293aa3dd4b57edd0f484fb1c046b2
Adrian Lippa <adrian.lippa@gmail.comm>
2017-03-19 19:43:58 +0100

    Menu ostylowane i sprawny scroll

--- swapp/src/navbar/index.js
+++ swapp/src/navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import Swapp from '../img/logo.svg';
 
 var ReactDOM  = require('react-dom');
 var Scroll    = require('react-scroll');
@@ -11,9 +12,6 @@ var Events     = Scroll.Events;
 var scroll     = Scroll.animateScroll;
 var scrollSpy  = Scroll.scrollSpy;
 
-var durationFn = function(deltaTop) {
-    return deltaTop;
-};
 
 var NavigationBar = React.createClass({
     componentDidMount: function() {
@@ -38,30 +36,20 @@ var NavigationBar = React.createClass({
     },
     render: function () {
         return (
-            <div>
-                <nav className="navbar navbar-default navbar-fixed-top">
-                    <div className="container-fluid">
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul className="nav navbar-nav">
-                                <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Test 1</Link></li>
-                                <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500}>Test 2</Link></li>
-                                <li><Link activeClass="active" className="test3" to="test3" spy={true} smooth={true} duration={500} >Test 3</Link></li>
-                                <li><Link activeClass="active" className="test4" to="test4" spy={true} smooth={true} duration={500}>Test 4</Link></li>
-                                <li><Link activeClass="active" className="test5" to="test5" spy={true} smooth={true} duration={500} delay={1000}>Test 5 ( delay )</Link></li>
-                                <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>Test 6 (anchor)</Link></li>
-                                <li><Link activeClass="active" className="test7" to="test7" spy={true} smooth={true} duration={durationFn}>Test 7 (duration and container)</Link></li>
-                                <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
-                                <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
-                                <li> <a onClick={() => scroll.scrollMore(500)}>Scroll 500 More!</a></li>
-                                <li> <a onClick={() => scroll.scrollMore(1000, { delay : 1500 })}>Scroll 1000 More! ( delay ) </a></li>
-                                <li><Link activeClass="active" className="test8" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
-                                <li><Link activeClass="active" className="test9" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
+            <div className="">
+                <nav className="container-fluid navbar navbar-default navbar-fixed-top ">
+                    <div className="container-fluid navMenu">
+                        <div className="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+                            <img className="logo navbar-left" src={Swapp} onClick={() => scroll.scrollToTop()} alt="SWAPP"/>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><Link activeClass="active" onClick={() => scroll.scrollToTop()} className="linkMod" to="teaser" spy={true} smooth={true} duration={500} >Start</Link></li>
+                                <li><Link activeClass="active" onClick={() => scroll.scrollTo(850)} className="linkMod" to="test2" spy={true} smooth={true} duration={500}>Lista Funkcji</Link></li>
+                                <li><Link activeClass="active" onClick={() => scroll.scrollToBottom()} className="linkMod" to="test3" spy={true} smooth={true} duration={500} >O Nas!</Link></li>
                             </ul>
                         </div>
                     </div>
                 </nav>
 
-                <a onClick={this.scrollToTop}>To the top!</a>
 
             </div>
         );
