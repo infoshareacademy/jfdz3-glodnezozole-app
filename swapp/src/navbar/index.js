ed4ee3f479c3ed3ef2a37d0f9671c908b84bde51
Adrian Lippa <adrian.lippa@gmail.comm>
2017-03-19 20:18:58 +0100

    Drobne poprawki

--- swapp/src/navbar/index.js
+++ swapp/src/navbar/index.js
@@ -2,38 +2,13 @@ import React from 'react';
 import './index.css';
 import Swapp from '../img/logo.svg';
 
-var ReactDOM  = require('react-dom');
 var Scroll    = require('react-scroll');
-
 var Link       = Scroll.Link;
-var DirectLink = Scroll.DirectLink;
-var Element    = Scroll.Element;
-var Events     = Scroll.Events;
 var scroll     = Scroll.animateScroll;
-var scrollSpy  = Scroll.scrollSpy;
 
 
 var NavigationBar = React.createClass({
-    componentDidMount: function() {
 
-        Events.scrollEvent.register('begin', function() {
-            console.log("begin", arguments);
-        });
-
-        Events.scrollEvent.register('end', function() {
-            console.log("end", arguments);
-        });
-
-        scrollSpy.update();
-
-    },
-    scrollToTop: function() {
-        scroll.scrollToTop();
-    },
-    componentWillUnmount: function() {
-        Events.scrollEvent.remove('begin');
-        Events.scrollEvent.remove('end');
-    },
     render: function () {
         return (
             <div className="">
@@ -42,9 +17,9 @@ var NavigationBar = React.createClass({
                         <div className="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
                             <img className="logo navbar-left" src={Swapp} onClick={() => scroll.scrollToTop()} alt="SWAPP"/>
                             <ul className="nav navbar-nav navbar-right">
-                                <li><Link activeClass="active" onClick={() => scroll.scrollToTop()} className="linkMod" to="teaser" spy={true} smooth={true} duration={500} >Start</Link></li>
-                                <li><Link activeClass="active" onClick={() => scroll.scrollTo(850)} className="linkMod" to="test2" spy={true} smooth={true} duration={500}>Lista Funkcji</Link></li>
-                                <li><Link activeClass="active" onClick={() => scroll.scrollToBottom()} className="linkMod" to="test3" spy={true} smooth={true} duration={500} >O Nas!</Link></li>
+                                <li><Link activeClass="active" onClick={() => scroll.scrollToTop()} className="linkMod" to="Teaser" spy={true} smooth={true} duration={500} >Start</Link></li>
+                                <li><Link activeClass="active" onClick={() => scroll.scrollTo(850)} className="linkMod" to="FunctionList" spy={true} smooth={true} duration={500}>Lista Funkcji</Link></li>
+                                <li><Link activeClass="active" onClick={() => scroll.scrollToBottom()} className="linkMod" to="AboutUs" spy={true} smooth={true} duration={500} >O Nas!</Link></li>
                             </ul>
                         </div>
                     </div>
