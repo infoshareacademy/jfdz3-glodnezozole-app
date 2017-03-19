947eaa73117e1c2643dd8b14bd1055334e93850d
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2017-03-19 19:30:37 +0100

    log screen work in progress

--- swapp/src/App.js
+++ swapp/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import FunctionList from './function_list';
 import AboutUs from './about_us/about_us';
+import LogScreen from './log_screen/log_screen'
+
 class App extends Component {
     render() {
         return (
             <div>
                 <FunctionList/>
                 <AboutUs/>
+                <LogScreen/>
             </div>
         );
     }
