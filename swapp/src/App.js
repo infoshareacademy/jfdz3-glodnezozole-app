e87c8e3ac086913aeb8bb38ecedcd86f422b285c
sledziu87 <sledzinski.bartosz@gmail.com>
2017-03-19 12:46:03 +0100

    JFDZ3GZ-63 Wprowadzenie sekcji Teaser; Załączenie jej do App.js

--- swapp/src/App.js
+++ swapp/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
+import Teaser from './teaser/teaser';
 class App extends Component {
   render() {
     return (
-        <div></div>
+        <div>
+            <Teaser/>
+        </div>
     );
   }
 }
---
+++ swapp/src/teaser/teaser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './teaser.css';
+//import obrazków
+
+function Teaser(props) {
+    // return (
+    // //    treść html
+    // )
+}
+
+export default Teaser;
